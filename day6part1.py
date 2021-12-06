a3c7c59a907410f9e0d9c05b7c53392cb17bb173
Patrick Fach-Pedersen <patrick@fach-pedersen.net>
2021-12-06 17:05:47 +0100

    1 more day

---
+++ day6part1.py
@@ -0,0 +1,22 @@
+import re
+
+EXPECTED_RESULT = 5934
+BASE_NAME = re.search('day[0-9]+', __file__)[0]
+DEMO_FILE = f"{BASE_NAME}demo.txt"
+PUZZLE_FILE = f"{BASE_NAME}input.txt"
+
+def puzzle(filename):
+    with open(filename) as f:
+        line = list(map(int, re.split(',', f.readline().rstrip())))
+        timers = [0] * 9
+        for fish in line:
+            timers[fish] += 1
+        for day in range(80):
+            timers.append(timers.pop(0))
+            timers[6] += timers[8]
+    return sum(timers)
+
+if (d := puzzle(DEMO_FILE)) == EXPECTED_RESULT:
+    print(puzzle(PUZZLE_FILE))
+else:
+    print(f'Oh no! Your demo got {d} instead of {EXPECTED_RESULT}!')
